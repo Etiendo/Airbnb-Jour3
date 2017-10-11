b4d3d7628a235267293d7a6105ea39ac4f51df66
Orelia Sokambi <orelia.sokambi@gmail.com>
2017-10-11 17:07:09 +0200

    ajout parties orelia

--- foundation-6.4.2-complete/js/app.js
+++ foundation-6.4.2-complete/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+
+window.prettyPrint && prettyPrint();
+
+$('#dp2').fdatepicker({
+	initialDate: '11-10-2017',
+	format: 'dd-mm-yyyy',
+	disableDblClickSelection: true,
+	leftArrow:'<',
+	rightArrow:'>',
+	closeIcon:'X',
+	closeButton: true
+});
